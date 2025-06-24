d2df648bbdac592196a62e3ea155658c86413819
huenguyensw <hue.blue@gmail.com>
2025-06-24 20:21:26 +0200

    correct error of authentication

--- Program.cs
+++ Program.cs
@@ -187,9 +187,11 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
-app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
+
 
 
 app.Run();
